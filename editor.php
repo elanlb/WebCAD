9ee867a9ec8ed91bfdfc0ef2e93050cc40e32593
Elan <elanbustos@gmail.com>
2018-01-07 20:39:15 -0800

    Add files via upload

---
+++ editor.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>WebCAD Editor</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	</head>
+	<body>
+		<?php
+		echo file_get_contents("navbar.php");
+		?>
+		
+		<div class="container">
+			<div class="row">
+				<h1>Editor</h1>
+			</div>
+			<div class="row">
+				<div class="col-8" id="editor"></div>
+				<div class="col-4">
+					<div class="container" id="controls">
+						<h2>Controls</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<code id="json">
+					<?php
+					echo '{
+"shapes":[
+	{"name":"BigCube", "shape":"cube", "center":[-0.25, -0.25, -0.25]},
+	{"name":"Sphere", "shape":"sphere", "center":[0.25, 0.25, 0.25], "radius":1.3}
+],
+"operations":[
+	{"operation":"subtract", "object":1}
+]
+}';
+					?>
+				</code>
+			</div>
+		</div>
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		
+		<!-- Load csg then GL stuff and then the editor script -->
+		<script src="https://rawgit.com/evanw/csg.js/master/csg.js"></script>
+		<script src="js/threeGeometry.js"></script>
+		
+		<script src="https://rawgit.com/mrdoob/three.js/dev/build/three.js"></script>
+		<script src="https://rawgit.com/mrdoob/three.js/dev/examples/js/Detector.js"></script>
+		<script src="https://rawgit.com/mrdoob/three.js/dev/examples/js/controls/OrbitControls.js"></script>
+		
+		<script src="js/editor.js"></script>
+	</body>
+</html>
