c286ce1eaf8e1f3ea2f1f67fdffc09e8deeea8bf
Elan Bustos <elanbustos@gmail.com>
2018-01-25 20:01:25 -0800

    Link to GitHub page

--- navbar.php
+++ navbar.php
@@ -3,13 +3,14 @@
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	
+
 	<div class="collapse navbar-collapse" id="navbarDropdown">
 		<div class="navbar-nav">
 			<a class="nav-item nav-link" href="/index.php">Home</a>
 			<a class="nav-item nav-link" href="/editor.php">Editor</a>
 			<a class="nav-item nav-link" href="/about.php">About</a>
 			<a class="nav-item nav-link" href="/account.php">Account</a>
+			<a class="nav-item nav-link" href="https://github.com/elanlb/WebCAD">GitHub</a>
 		</div>
 	</div>
 </nav>
